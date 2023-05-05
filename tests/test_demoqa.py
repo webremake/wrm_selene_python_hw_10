7d6d832dcaa3fa87af5a6a2213b382787a34f6fa
Heorhii Lazarevich <webremake@gmail.com>
2023-05-05 20:19:49 +0300

    refactor code using Fluent PageObject pattern

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,6 +1,5 @@
-from selene import browser, have, be
-
 from wrm_selene_python_hw_10.model.pages.registration_page import RegistrationPage
+
 
 def test_student_registration_form(browser_control):
     # GIVEN
@@ -8,36 +7,34 @@ def test_student_registration_form(browser_control):
     registration_page.open()
 
     # WHEN
-    registration_page.fill_first_name('Jon')
-    registration_page.fill_last_name('Dir')
-    registration_page.fill_email('jondir@example.com')
-    registration_page.select_gender('Male')
-    registration_page.fill_mobile_phone('5296846163')
-    registration_page.fill_date_of_birth('1957', 'May', '12')
-    registration_page.fill_subjects('Commerce')
-    registration_page.select_hobbies('Reading')
-    registration_page.upload_picture('gl.jpg')
-    registration_page.fill_current_address('This is my current address in New York USA')
-    registration_page.select_state('Haryana')
-    registration_page.select_city('Karnal')
-    registration_page.submit()
+    registration_page\
+        .fill_first_name('Jon').fill_last_name('Dir')\
+        .fill_email('jondir@example.com')\
+        .select_gender('Male')\
+        .fill_mobile_phone('5296846163')\
+        .fill_date_of_birth('1957', 'May', '12')\
+        .fill_subjects('Commerce')\
+        .select_hobbies('Reading')\
+        .upload_picture('gl.jpg')\
+        .fill_current_address('This is my current address in New York USA')\
+        .select_state('Haryana').select_city('Karnal')\
+        .click_submit()
 
     # THEN
-    # section CHECK IFRAME TABLE
-    # check table header has two columns
-    browser.all('.table thead>tr>th').should(have.size(2))
-
-    # check table header cells have correct text
-    browser.all('.table thead>tr>th').should(have.exact_texts('Label', 'Values'))
-
-    # check table body has ten columns
-    browser.all('.table tbody>tr').should(have.size(10))
-
-    # check table cells in column Value have values entered into the form in the previous steps
-    browser.all('.table tbody>tr>td').even.should(have.exact_texts(
-        'Jon Dir', 'jondir@example.com', 'Male', '5296846163', '12 May,1957', 'Commerce',
-        'Reading', 'gl.jpg', 'This is my current address in New York USA', 'Haryana Karnal'))
-    # end section
-
-    # close iframe table
-    browser.element('#closeLargeModal').should(be.clickable).click()
+    registration_page\
+        .should_thanks_frame_have_title('Thanks for submitting the form')\
+        .should_thanks_frame_table_have_columns(2)\
+        .should_thanks_frame_table_have_header_sells('Label', 'Values')\
+        .should_thanks_frame_table_have_rows(10)\
+        .should_have_registered_user_info(
+            'Jon Dir',
+            'jondir@example.com',
+            'Male',
+            '5296846163',
+            '12 May,1957',
+            'Commerce',
+            'Reading',
+            'gl.jpg',
+            'This is my current address in New York USA',
+            'Haryana Karnal')\
+        .close_thanks_frame()
--- wrm_selene_python_hw_10/model/pages/registration_page.py
+++ wrm_selene_python_hw_10/model/pages/registration_page.py
@@ -14,54 +14,96 @@ class RegistrationPage:
         browser.element('.sidebar-content').execute_script('element.remove()')
 
         browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        return self
 
     def fill_first_name(self, value):
         browser.element('#firstName').should(be.blank).type(value)
+        return self
 
     def fill_last_name(self, value):
         browser.element('#lastName').should(be.blank).type(value)
+        return self
 
     def fill_email(self, address):
         browser.element('#userEmail').should(be.blank).type(address)
+        return self
 
     def select_gender(self, value):
         browser.all('[for^=gender-radio]').element_by(have.text(value)).click()
+        return self
 
     def fill_mobile_phone(self, number):
         browser.element('#userNumber').should(be.blank).type(number)
+        return self
 
     def fill_date_of_birth(self, year, month, day):
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').type(month)
         browser.element('.react-datepicker__year-select').type(year)
         browser.element(f'.react-datepicker__day--0{day}').click()
+        return self
 
     def fill_subjects(self, text):
         browser.element('#subjectsInput').type(text).press_enter()
+        return self
 
     def select_hobbies(self, value):
         browser.all('[for^=hobbies-checkbox]').element_by(
             have.text(value)).should(be.clickable).click()
+        return self
 
     def upload_picture(self, file):
         browser.element('#uploadPicture').send_keys(path(file))
+        return self
 
     def fill_current_address(self, text):
         browser.element('#currentAddress').should(be.blank).with_(set_value_by_js=True).set_value(
             text
         )
+        return self
 
     def select_state(self, name):
         browser.element('#react-select-3-input').send_keys("a")
         browser.all('[id^="react-select-3-option"]').element_by(
             have.exact_text(name)
         ).click()
+        return self
 
     def select_city(self, name):
         browser.element('#react-select-4-input').send_keys("a")
         browser.all('[id^="react-select-4-option"]').element_by(
             have.exact_text(name)
         ).click()
+        return self
 
-    def submit(self):
+    def click_submit(self):
         browser.element('#submit').with_(click_by_js=True).click()
+        return self
+
+    def should_thanks_frame_have_title(self, text):
+        browser.element('.modal-header').should(have.text(text))
+        return self
+
+    def should_thanks_frame_table_have_columns(self, column_number: int):
+        browser.all('.table thead>tr>th').should(have.size(column_number))
+        return self
+
+    def should_thanks_frame_table_have_header_sells(self, sell1, sell2):
+        browser.all('.table thead>tr>th').should(have.exact_texts(sell1, sell2))
+        return self
+
+    def should_thanks_frame_table_have_rows(self, rows_number: int):
+        browser.all('.table tbody>tr').should(have.size(rows_number))
+        return self
+
+    def should_have_registered_user_info(self, full_name, email, gender, mobile,
+                                         date_of_birth, subjects, hobbies,
+                                         picture, address, state_and_city):
+        browser.all('.table tbody>tr>td').even.should(have.exact_texts(
+            full_name, email, gender, mobile, date_of_birth, subjects,
+            hobbies, picture, address, state_and_city))
+        return self
+
+    def close_thanks_frame(self):
+        browser.element('#closeLargeModal').should(be.clickable).click()
+        return self
