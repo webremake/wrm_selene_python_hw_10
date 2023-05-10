cce04f693b7c476e61f0e3631b0a5d130fd0e869
Heorhii Lazarevich <webremake@gmail.com>
2023-05-10 15:58:03 +0300

    add method should_have_data() to tests

--- tests/test_text_box.py
+++ tests/test_text_box.py
@@ -1,6 +1,6 @@
-from selene import browser, have
 from wrm_selene_python_hw_10.model.pages.text_box_page import TextBox
 from wrm_selene_python_hw_10.data import users
+
 
 def test_text_box(browser_control):
     # GIVEN
@@ -11,7 +11,4 @@ def test_text_box(browser_control):
     text_box.register(users.student)
 
     # THEN
-    browser.element('#output #name').should(have.text(users.student.full_name))
-    browser.element('#output #email').should(have.text(users.student.email))
-    browser.element('#output #currentAddress').should(have.text(users.student.current_address))
-    browser.element('#output #permanentAddress').should(have.text(users.student.current_address))
+    text_box.should_have_data(users.student)
