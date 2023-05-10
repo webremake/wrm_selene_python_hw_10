98c178d035cee84f2a8f85e65b3b24171df8cf8c
Heorhii Lazarevich <webremake@gmail.com>
2023-05-11 01:30:47 +0300

    implement MenuPanel and Application classes methods to test function test_text_box()

--- tests/test_text_box.py
+++ tests/test_text_box.py
@@ -1,14 +1,14 @@
-from wrm_selene_python_hw_10.model.pages.text_box_page import TextBox
+from wrm_selene_python_hw_10.application import app
 from wrm_selene_python_hw_10.data import users
 
 
 def test_text_box(browser_control):
     # GIVEN
-    text_box = TextBox()
-    text_box.open()
+    app.open()
+    app.menu_panel.open_panel_menu_item(link_text='Text Box')
 
     # WHEN
-    text_box.register(users.student)
+    app.text_box.register(users.student)
 
     # THEN
-    text_box.should_have_data(users.student)
+    app.text_box.should_have_data(users.student)
