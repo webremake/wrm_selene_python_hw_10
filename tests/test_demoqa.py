ac9181ac9b73aeb5a098b841c85fe643326ce484
Heorhii Lazarevich <webremake@gmail.com>
2023-05-13 20:48:53 +0300

    removed registration_page object creation from function

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,9 +1,10 @@
 from wrm_selene_python_hw_10.model.pages.registration_page import RegistrationPage
 
+registration_page = RegistrationPage()
+
 
 def test_student_registration_form(browser_control):
     # GIVEN
-    registration_page = RegistrationPage()
     registration_page.open()
 
     # WHEN
